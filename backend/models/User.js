36c2ffca6591deee557024ae25f92e28ef6ac78c
lxakshaseth <lxakshatseth90@gmail.com>
2026-08-24 03:53:03 +0530

    perf(backend): add mongodb indexing, efficient pagination and query optimization

--- backend/models/User.js
+++ backend/models/User.js
@@ -29,11 +29,34 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    bio: {
+      type: String,
+      maxlength: 160,
+      default: "",
+    },
+    location: {
+      type: String,
+      maxlength: 60,
+      default: "",
+    },
+    website: {
+      type: String,
+      maxlength: 100,
+      default: "",
+    },
+    savedPosts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
   },
   {
     timestamps: true,
   }
 );
+
+userSchema.index({ createdAt: -1 });
 
 module.exports = mongoose.model("User", userSchema);
 
